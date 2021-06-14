69ca87f0e2767c62f9f4cfb5e585b2bb8edadeb4
Viklysx <vikbelec111@yandex.ru>
2021-06-14 03:02:30 +0300

    feat: add component

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function useJsonFetch(url) {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setData(data);
+        setError(null);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return [data, isLoading, error];
+}
+
+function CostumHook({ url }) {
+  const [data, isLoading, error] = useJsonFetch(url);
+  return (
+    <div className="block">
+      <div>{data && data.status}</div>
+      <div>{error && error.message}</div>
+      <div>{isLoading && "Загрузка..."}</div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CostumHook url={"http://localhost:7777/data"} />
+      <CostumHook url={"http://localhost:7777/error"} />
+      <CostumHook url={"http://localhost:7777/loading"} />
     </div>
   );
 }
